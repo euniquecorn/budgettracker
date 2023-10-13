26bb9677a7951da95da4f284d12aeab3b00da1dc
Roland G. Barro Jr. <rolandbarro@gmail.com>
2023-10-13 11:21:15 +0800

    file management

---
+++ lib/auth/register.dart
@@ -0,0 +1,151 @@
+import 'package:budget_tracker/firebase/authenticator.dart';
+import 'package:budget_tracker/users.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class Register extends StatefulWidget {
+  const Register({super.key});
+
+  @override
+  State<Register> createState() => _RegisterState();
+}
+
+class _RegisterState extends State<Register> {
+  TextEditingController namecontroller = TextEditingController();
+  TextEditingController emailcontroller = TextEditingController();
+  TextEditingController passwordcontroller = TextEditingController();
+  late String errormessage;
+  late bool isError;
+  @override
+  void initState() {
+    errormessage = "This is an error";
+    isError = false;
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          margin: const EdgeInsets.symmetric(horizontal: 25),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'REGISTER',
+                style: txtstyle,
+              ),
+              const SizedBox(height: 15),
+              TextField(
+                controller: namecontroller,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Enter name',
+                  prefixIcon: Icon(Icons.person),
+                ),
+              ),
+              const SizedBox(height: 15),
+              TextField(
+                controller: emailcontroller,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Enter Email Address',
+                  prefixIcon: Icon(Icons.email),
+                ),
+              ),
+              const SizedBox(height: 15),
+              TextField(
+                obscureText: true,
+                controller: passwordcontroller,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Enter password',
+                  prefixIcon: Icon(Icons.lock),
+                ),
+              ),
+              const SizedBox(height: 15),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  minimumSize: const Size.fromHeight(50),
+                ),
+                onPressed: () {
+                  registerUser();
+                },
+                child: const Text('REGISTER'),
+              ),
+              const SizedBox(height: 15),
+              (isError)
+                  ? Text(
+                      errormessage,
+                      style: errortxtstyle,
+                    )
+                  : Container(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  var errortxtstyle = const TextStyle(
+    fontWeight: FontWeight.bold,
+    color: Colors.red,
+    letterSpacing: 1,
+    fontSize: 18,
+  );
+  var txtstyle = const TextStyle(
+    fontWeight: FontWeight.bold,
+    letterSpacing: 2,
+    fontSize: 38,
+  );
+
+  Future createUser() async {
+    final user = FirebaseAuth.instance.currentUser!;
+    final userid = user.uid;
+    final docUser = FirebaseFirestore.instance.collection('Users').doc(userid);
+
+    final users = Users(
+      id: userid,
+      name: namecontroller.text,
+      email: emailcontroller.text,
+    );
+
+    final json = users.toJson();
+    await docUser.set(json);
+
+    goToAuthenticator();
+  }
+
+  Future registerUser() async {
+    try {
+      await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: emailcontroller.text.trim(),
+        password: passwordcontroller.text.trim(),
+      );
+      createUser();
+      setState(() {
+        errormessage = "";
+      });
+    } on FirebaseAuthException catch (e) {
+      print(e);
+      setState(() {
+        errormessage = e.message.toString();
+      });
+    }
+  }
+
+  goToAuthenticator() {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => const Authenticator(),
+      ),
+    );
+  }
+}
