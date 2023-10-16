9e2bf80d19846ae428f016ccecb5b85e0c4a1081
Roland G. Barro Jr. <rolandbarro@gmail.com>
2023-10-16 12:09:37 +0800

    user profile page

--- lib/user/userProfile_page.dart
+++ lib/user/userProfile_page.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class UserProfilePage extends StatefulWidget {
@@ -8,6 +10,18 @@ class UserProfilePage extends StatefulWidget {
 }
 
 class _UserProfilePageState extends State<UserProfilePage> {
+  TextEditingController namecontroller = TextEditingController();
+  TextEditingController emailcontroller = TextEditingController();
+  late String errormessage;
+  late bool isError;
+
+  @override
+  void initState() {
+    errormessage = "This is an error";
+    isError = false;
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,6 +29,83 @@ class _UserProfilePageState extends State<UserProfilePage> {
         backgroundColor: const Color.fromARGB(255, 195, 171, 235),
         title: const Text('User Profile'),
       ),
+      body: Center(
+        child: Container(
+          margin: const EdgeInsets.symmetric(horizontal: 25),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'USER PROFILE',
+                style: txtstyle,
+              ),
+              const SizedBox(height: 15),
+              TextField(
+                controller: namecontroller,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'name',
+                  prefixIcon: Icon(Icons.person),
+                ),
+              ),
+              const SizedBox(height: 15),
+              TextField(
+                controller: emailcontroller,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Email Address',
+                  prefixIcon: Icon(Icons.email),
+                ),
+              ),
+              const SizedBox(height: 15),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  minimumSize: const Size.fromHeight(50),
+                ),
+                onPressed: () {
+                  saveUserDetails();
+                },
+                child: const Text('Save'),
+              ),
+              const SizedBox(height: 15),
+              (isError)
+                  ? Text(
+                      errormessage,
+                      style: errortxtstyle,
+                    )
+                  : Container(),
+              const SizedBox(height: 15),
+            ],
+          ),
+        ),
+      ),
     );
+  }
+
+  var errortxtstyle = const TextStyle(
+    fontWeight: FontWeight.bold,
+    color: Colors.red,
+    letterSpacing: 1,
+    fontSize: 18,
+  );
+  var txtstyle = const TextStyle(
+    fontWeight: FontWeight.bold,
+    letterSpacing: 2,
+    fontSize: 38,
+  );
+
+  Future saveUserDetails() async {
+    try {
+      print('Save User Details...');
+      // await FirebaseAuth.instance.createUserWithEmailAndPassword(
+      //   email: emailcontroller.text.trim(),
+      //   password: passwordcontroller.text.trim(),
+      // );
+    } on FirebaseAuthException catch (e) {
+      print(e);
+      setState(() {
+        errormessage = e.message.toString();
+      });
+    }
   }
 }
